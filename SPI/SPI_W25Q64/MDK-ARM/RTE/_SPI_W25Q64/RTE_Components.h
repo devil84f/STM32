c19bbf2e6538e9b74914a4b2b3d2fdbac3971621
LiNing <1204259355@qq.com>
2025-04-18 16:16:42 +0800

    LeeCode/25/4/18

---
+++ SPI/SPI_W25Q64/MDK-ARM/RTE/_SPI_W25Q64/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SPI_W25Q64' 
+ * Target:  'SPI_W25Q64' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ SPI/SPI_W25Q64/MDK-ARM/SPI_W25Q64/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+spi_w25q64\startup_stm32f103xb.o: startup_stm32f103xb.s
